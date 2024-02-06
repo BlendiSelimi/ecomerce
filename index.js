4b133e7577c4f10e1c0303a67eddff876bc39cfd
BlendiSelimi <blendselimi15@gmail.com>
2024-02-06 15:43:42 +0100

    made changes on the cards

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function myFunction() {
 
 
   
-  let slideIndex = 1;
+ let slideIndex = 1;
 showSlides(slideIndex);
 
 
@@ -25,9 +25,10 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+ 
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 1; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
